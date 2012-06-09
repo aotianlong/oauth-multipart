bccaff1c0f5e2c1cfd5c9551cf266fbd08d62931
aotianlong <aotianlong@gmail.com>
2012-06-09 23:18:54 +0800

    remove unused files and add oauth patch

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'b417820c518f0b533c87f12234e69a3e0ce2cf78dc92f6867f7972e4b381a9138ad199cdb49b2378caa45ffae2bec567a6d9bb3fe15bcadaf8a55faa1da021ee'
